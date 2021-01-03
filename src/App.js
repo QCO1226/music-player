1ea5c1b29efb25405e00fff58dde71ac37bb23e2
Miguel Bernal <miguel1226bernal@gmail.com>
2021-01-03 10:50:11 -0500

    Boilerplate del reproductor listo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Player from './components/Player';
+import Song from './components/Song';
+import './styles/app.scss';
+import data from './util';
+
 function App() {
   return (
     <div className="App">
-      
+      <Song />
+      <Player />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,19 @@
-import React from 'react';
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlay, faAngleLeft, faAngleRight, } from '@fortawesome/free-solid-svg-icons';
 const Player = () =>{
     return(
         <div className="player">
-            <h1>Player</h1>
+            <div className="time-control">
+                <p>Start Time</p>
+                <input type="range" />
+                <p>End Time</p>
+            </div>
+            <div className="play-control">
+                <FontAwesomeIcon className="play" size="2x" icon={faAngleLeft} />
+                <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon className="play" size="2x" icon={faAngleRight} />
+
+            </div>
         </div>
     )
 }
