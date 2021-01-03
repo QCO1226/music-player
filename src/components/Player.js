22aa77dd605298e1488095083eae30f958d746d0
Miguel Bernal <miguel1226bernal@gmail.com>
2021-01-03 11:14:31 -0500

    states creados para manejar los controles de reproduccion

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,19 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPlay, faAngleLeft, faAngleRight, } from '@fortawesome/free-solid-svg-icons';
-const Player = () =>{
+import { useRef } from 'react';
+const Player = ({currentSong, isPlaying, setIsPlaying}) =>{
+    //Ref
+    const audioRef = useRef(null);
+    //Event Handlers
+    const playSongHandler = () => {
+        if(isPlaying){
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying);
+        }else{
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
+    }
     return(
         <div className="player">
             <div className="time-control">
@@ -9,11 +22,12 @@ const Player = () =>{
                 <p>End Time</p>
             </div>
             <div className="play-control">
-                <FontAwesomeIcon className="play" size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
-                <FontAwesomeIcon className="play" size="2x" icon={faAngleRight} />
+                <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
 
             </div>
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
     )
 }
